fb74f9af97529a6c6029fcb7229c6a025f0da787
sahilpotdukhe <sahilpotdukhe.ssp@gmail.com>
2025-03-27 19:05:02 +0530

    Invoice page improvements (made general)

--- lib/Screens/AuthenticationScreens/SignUpScreen.dart
+++ lib/Screens/AuthenticationScreens/SignUpScreen.dart
@@ -44,10 +44,9 @@ class _SignUpScreenState extends State<SignUpScreen> {
               'https://firebasestorage.googleapis.com/v0/b/echat-888ef.appspot.com/o/user.jpg?alt=media&token=6f598b3e-9c10-49ee-8a68-49468b32bddf',
           authType: 'emailAuth',
           mobilenumber: mobileController.text,
-          qrImageUrl:
+          signatureImageUrl:
               'https://firebasestorage.googleapis.com/v0/b/fitstrong-gym.appspot.com/o/qrgeneral.png?alt=media&token=ffa3b5fe-ade2-4d7e-b675-bfdf2b281f5a',
           address: '',
-          signature: '',
           merchantName: '',
           upiId: '');
       AwesomeDialog(
